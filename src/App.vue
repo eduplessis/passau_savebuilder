6c5ac2fc2b6d097e81c6851ff39263e880d5d812
Edouard Duplessis <eduplessis@gmail.com>
2019-07-05 12:31:27 -0400

    save and image clean email

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,178 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="savebuilder">
+    <div class="shadow" v-if="open">
+      <div class="lightbox">
+        <button class="close" @click.prevent="open = false">fermer</button>
+        <h1>Vos équipements sauvegardés</h1>
+        <ul>
+          <li v-for="(save, key) in storage" :key="key">
+            <div class="thumb">
+              <img :src="save.selection.base" alt />
+              <div
+                class="color"
+                v-for="(color, keyColor) in save.selection.color"
+                :key="key + '-' + keyColor"
+              >
+                <img v-if="color.imageUrl" :src="color.imageUrl" />
+              </div>
+            </div>
+            <div>
+              <div class="caption">Équipement</div>
+              {{ save.selection.product }}
+            </div>
+            <div>
+              <div class="caption">Date de création</div>
+              {{ savedDate(save.date) }}
+            </div>
+            <a class="btnsave" @click.prevent="loadEquipement(save.selection)"
+              >Voir l'équipement</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <button class="triggersave btnsave" @click.prevent="open = !open">
+      Consultez vos équipements
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "app",
-  components: {
-    HelloWorld
+  name: "savebuilder",
+  data() {
+    return {
+      storage: false,
+      open: false
+    };
+  },
+  methods: {
+    loadStorage() {
+      if (localStorage.getItem("passau"))
+        this.storage = JSON.parse(localStorage.getItem("passau"));
+    },
+    savedDate(date) {
+      var local = document.querySelector("html");
+
+      var Newdate = new Date(date);
+      return Newdate.toLocaleDateString(local.lang, {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+      });
+    },
+    loadEquipement(selection) {
+      this.$cookie.set("loadpassau", JSON.stringify(selection), 1);
+      window.location.search =
+        "?p=11&step=2&type=" +
+        selection.type +
+        "&family=" +
+        selection.family +
+        "";
+    }
+  },
+  computed: {},
+  mounted() {
+    this.loadStorage();
   }
 };
 </script>
 
 <style lang="scss">
-#app {
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+.shadow {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: rgba(0, 0, 0, 0.2);
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9998;
+}
+.lightbox {
+  background: #fff;
+  margin: 0 auto;
+  padding: 30px;
+  max-width: 90vw;
+  width: 900px;
+  text-align: center;
+  position: fixed;
+  z-index: 99999;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  ul {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+  }
+  li {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    text-align: left;
+  }
+  .caption {
+    font-size: 700;
+  }
+}
+.btnsave {
+  background-color: #00adee;
+  padding: 15px 42px;
+  border-radius: 50px;
+  width: auto;
+  display: inline-block;
+  font-size: 17px;
+  font-weight: 300;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: center;
+  color: #ffffff;
+  cursor: pointer;
+  border: none;
+  &:hover {
+    background: #fff;
+    border: 1px solid #00adee;
+    color: #00adee;
+  }
+}
+.thumb {
+  width: 75px;
+  height: 75px;
+  background: hsl(0, 0, 85);
+  border-radius: 100%;
+  position: relative;
+
+  img {
+    width: 60%;
+    height: 60%;
+    object-fit: cover;
+    transform: translate(-50%, -50%);
+    left: 50%;
+    top: 50%;
+
+    position: absolute;
+  }
+}
+.triggersave {
+  position: fixed;
+  bottom: 20px;
+  left: 80px;
+  z-index: 9999;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import vueCookie from "vue-cookie";
 
 Vue.config.productionTip = false;
+Vue.use(vueCookie);
 
 new Vue({
   render: h => h(App)
-}).$mount("#app");
+}).$mount("#savebuilder");
